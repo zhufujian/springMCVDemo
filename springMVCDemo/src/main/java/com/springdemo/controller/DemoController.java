74c77749b38a9c913fced61661e978824fc5a5b6
zhufujian <zhu_fujian@163.com>
2018-10-19 11:15:12 +0800

    提交

---
+++ springMVCDemo/src/main/java/com/springdemo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "demo";
+	}
+
+}
